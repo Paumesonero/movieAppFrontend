d5cdc084ce6ef02ccadf607e963f52d3d67e61ad
Guillem <guillem.mart7@gmail.com>
2022-09-10 13:16:18 +0200

    view Home with searchbar included that links to /search

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
+    <div>
+      <h1>Find something to watch!</h1>
+      <form action="" method="GET" className="searchForm">
+        <button onClick={navigate("/search", {replace:true})}><input type="text" placeholder="Search"/><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+      </form>
+    </div>
   )
 }
