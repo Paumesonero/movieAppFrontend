fbf2e56ca6d793b7a91a7b47177d5361b9529dd7
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-12 13:40:09 +0200

    user can see ignored

--- src/views/votes/VoteList.jsx
+++ src/views/votes/VoteList.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeartCrack } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default function VoteList() {
     const[myVotes, setMyVotes] = useState(null)
+    const[ignored, setIgnored] = useState(false)
     const storedToken = localStorage.getItem('authToken');
 
     useEffect(() =>{
@@ -21,10 +23,18 @@ export default function VoteList() {
         }
         getVotes();
     },[storedToken]);
+
+    const handleCheck = (e) =>{
+        setIgnored(prev =>{
+            return !prev
+        })
+    }
     
   return (
     <div>
         <h2>Vote List</h2>
+        <label > See ignored</label>
+        <input type="checkbox" name='ignored' onChange={(e) => {handleCheck(e)}} />
         {myVotes && myVotes.map(el =>{
             return(
                 <div key={el._id}>
@@ -53,8 +63,25 @@ export default function VoteList() {
                         </div>
                     </div>
                     )}
-                    
+                  {(ignored && el.vote === undefined) &&(
+                    <div className='watchlist-movie'>
+                        <div className='poster-and-icon'>
+                            <img src={el.movieId.translations[0].poster.og} alt="movie" width='70px' className='movie-image' />
+                            <FontAwesomeIcon icon={faEyeSlash} className='ignore'/>
+                     </div>
+                     <div className='movie-info'>
+                            <p><strong>Title:</strong></p>
+                            <p>{el.movieId.name}</p>
+                            <p><strong>Release Date:</strong></p>
+                            <p>{el.movieId.premiere}</p>
+                            <p><strong>Average Rating:</strong></p>
+                            <p>{el.movieId.imdb_rating}</p>
+                            <Link to=''>Add Review</Link>
+                    </div>
+                    </div>
+                  )  }  
                 </div>
+                
                 
                 
             )
