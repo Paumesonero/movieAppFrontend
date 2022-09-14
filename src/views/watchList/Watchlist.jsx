c611ee960f73108d0fa777ad2015f6a1b80bc1f2
Guillem <guillem.mart7@gmail.com>
2022-09-14 20:12:12 +0200

    remove from watchlist button added

--- src/views/watchList/Watchlist.jsx
+++ src/views/watchList/Watchlist.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Watchlist() {
     const[myWatchList, setMyWatchList] = useState(null)
     const storedToken = localStorage.getItem('authToken');
-
+    const [errorMessage, setErrorMessage] = useState(undefined);
     useEffect(() =>{
         const getMovies = async() =>{
             try {
@@ -17,27 +17,36 @@ export default function Watchlist() {
         }
         getMovies();
     },[storedToken]);
-  return (
-    <div >
-        <h2>Your Watchlist</h2>
-        {myWatchList && myWatchList.map(el =>{
-            return(
-                <div key={el._id} className='watchlist-movie' >
-                    <div>
-                        <img src={el.movieId.translations[0].poster.og} alt="movie poster" width='70px' />
+    const handleRemove = async (movieId) => {
+        try {
+            await axios.delete(`${process.env.REACT_APP_API_URL}/watchlist/${movieId}/remove`, { headers: { Authorization: `Bearer ${storedToken}` } });
+            const moviesFromApi = await axios.get(`${process.env.REACT_APP_API_URL}/watchlist`, { headers: { Authorization: `Bearer ${storedToken}` } });
+            setMyWatchList(moviesFromApi.data.data);
+        } catch (error) {
+            setErrorMessage(error.response.data.error);
+        }
+    }
+    return (
+        <div >
+            <h2>Your Watchlist</h2>
+            {myWatchList && myWatchList.map(el =>{
+                return(
+                    <div key={el._id} className='watchlist-movie' >
+                        <div>
+                            <a href={`/movies/${el.movieId._id}`}><img src={el.movieId.translations[0].poster.og} alt="movie poster" width='70px' /></a>
+                        </div>
+                        <div className='movie-info'>
+                            <p><strong>Title:</strong></p>
+                            <p>{el.movieId.name}</p>
+                            <p><strong>Release Date:</strong></p>
+                            <p>{el.movieId.premiere}</p>
+                            <p><strong>Average Rating:</strong></p>
+                            <p>{el.movieId.imdb_rating}</p>
+                            <button onClick={() => handleRemove(el.movieId._id)}>Remove</button>
+                        </div>
                     </div>
-                    <div className='movie-info'>
-                        <p><strong>Title:</strong></p>
-                        <p>{el.movieId.name}</p>
-                        <p><strong>Release Date:</strong></p>
-                        <p>{el.movieId.premiere}</p>
-                        <p><strong>Average Rating:</strong></p>
-                        <p>{el.movieId.imdb_rating}</p>
-                        <Link to={`/movies/${el.movieId._id}`}>Read more</Link>
-                    </div>
-                </div>
-            )
-        })}
-    </div>
-  )
+                )
+            })}
+        </div>
+    )
 }
