6c50cbe44ad5b34c9454fb652b6e1fb5e4559f92
Guillem <guillem.mart7@gmail.com>
2022-09-14 19:03:24 +0200

    added all search views

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -6,26 +6,61 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
     const [search, setSearch] = useState("");
+    const [searchAPIName, setSearchAPIName] = useState("");
+    const [searchAPIImdbId, setSearchAPIImdbId] = useState("");
     const [errorMessage, setErrorMessage] = useState(undefined);
     const navigate = useNavigate();
     const handleChange = (e) => {
         setSearch(e.target.value)
-    };    
+    };
+    const handleChangeAPIName = (e) => {
+        setSearchAPIName(e.target.value);
+    };
+    const handleChangeAPIImdbId = (e) => {
+        setSearchAPIImdbId(e.target.value);
+    };   
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const searchedMovie = await axios.get(`${process.env.REACT_APP_API_URL}/movies/search/${search}`);
             navigate(`/movies/${searchedMovie.data.data[0]._id}`);
-            console.log(searchedMovie.data.data)
+        } catch (error) {
+            setErrorMessage(error.response.data.error)
+        }
+    };
+    const handleAPINameSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const searchedMovie = await axios.get(`${process.env.REACT_APP_API_URL}/movies/api-search-by-name/${searchAPIName}`);
+            console.log(JSON.stringify(searchedMovie.data.data));
         } catch (error) {
             console.log(error)
         }
-    };
+    }
+    const handleAPIImdbIdSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const searchedMovie = await axios.get(`${process.env.REACT_APP_API_URL}/movies/api-search-by-imdbId/${searchAPIImdbId}`);
+            console.log(JSON.stringify(searchedMovie.data.data));
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div>
             <form onSubmit={handleSubmit} method="GET" className="searchForm">
                 <input type="text" placeholder="Search" value={search} onChange={handleChange}/>
                 <input type="checkbox"/>
+                <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            </form>
+            <form onSubmit={handleAPINameSubmit} method="GET" className="searchForm">
+                <input type="text" placeholder="Search in API" value={searchAPIName} onChange={handleChangeAPIName}/>
+                <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            </form>
+            <form onSubmit={handleAPIImdbIdSubmit} method="GET" className="searchForm">
+                <input type="text" placeholder="Search in API" value={searchAPIImdbId} onChange={handleChangeAPIImdbId}/>
                 <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
                 {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
             </form>
