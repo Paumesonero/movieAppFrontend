4179cad5fe42bb093e778f42e659092c70d463a4
Guillem <guillem.mart7@gmail.com>
2022-09-16 19:28:50 +0200

    preferences are now stored and showed properly

--- src/views/user/Preferences.jsx
+++ src/views/user/Preferences.jsx
@@ -7,13 +7,12 @@ import { useEffect } from "react";
 
 export default function Preferences() {
   const { user } = useContext(AuthContext);
-  console.log(user);
   const storedToken = localStorage.getItem('authToken');
   const navigate = useNavigate();
   const [genres, setGenres] = useState({
-    action: true,
-    // drama: user.preferences.drama,
-    // fantasy: user.preferences.fantasy,
+    action: false,
+    drama: false,
+    fantasy: false,
     // comedy: user.preferences.comedy,
     // mystery: user.preferences.mystery,
     // adventure: user.preferences.adventure,
@@ -24,18 +23,71 @@ export default function Preferences() {
     // documentary: user.preferences.documentary,
     // crime: user.preferences.crime,
   });
-  const handleChange = (e) => {
-    console.log(e.target.name);
+  useEffect(() => {
+    const getPreferences = async () => {
+      try {
+        const currentUser = await axios.get(`${process.env.REACT_APP_API_URL}/user/loggedInUser`, { headers: { Authorization: `Bearer ${storedToken}` } });
+        setGenres({
+          action: currentUser.data.data.preferences.includes("1"),
+          drama: currentUser.data.data.preferences.includes("12"),
+          fantasy: currentUser.data.data.preferences.includes("14"),
+
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPreferences();
+  },[storedToken]);
+  const handleCheck = (e) => {
     setGenres(prev => {
       return{
         ...prev,
-        [e.target.name]:!e.target.name
+        [e.target.name]:e.target.checked
       }
     });
   };
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPreferences = [];
+    if(genres.action) {
+        newPreferences.push("1");
+    };
+    if(genres.drama) {
+        newPreferences.push("12");
+    };
+    if(genres.fantasy) {
+        newPreferences.push("14");
+    };
+    // if(comedy === true) {
+    //     newPreferences.push("8");
+    // };
+    // if(mystery === true) {
+    //     newPreferences.push("22");
+    // };
+    // if(adventure === true) {
+    //     newPreferences.push("3");
+    // };
+    // if(war === true) {
+    //     newPreferences.push("34");
+    // };
+    // if(scifi === true) {
+    //     newPreferences.push("27");
+    // };
+    // if(romance === true) {
+    //     newPreferences.push("26");
+    // };
+    // if(history === true) {
+    //     newPreferences.push("20");
+    // };
+    // if(documentary === true) {
+    //     newPreferences.push("11");
+    // };
+    // if(crime === true) {
+    //     newPreferences.push("10");
+    // };
     try {
-      await axios.put(`${process.env.REACT_APP_API_URL}/user/preferences`, genres, { headers: { Authorization: `Bearer ${storedToken}` } });
+      await axios.put(`${process.env.REACT_APP_API_URL}/user/preferences`, newPreferences, { headers: { Authorization: `Bearer ${storedToken}` } });
       toast.success('Your preferences have been udpated.');
       // navigate('/');
     } catch (error) {
@@ -47,7 +99,11 @@ export default function Preferences() {
       {user && <div>
         <form onSubmit={handleSubmit} action="">
           <label>Action</label>
-          <input type="checkbox" className="checkbox" id="action" name="action" checked={genres.action} onChange={handleChange} />
+          <input type="checkbox" className="checkbox" id="action" name="action" checked={genres.action} onChange={(e) => handleCheck(e)} />
+          <label>Drama</label>
+          <input type="checkbox" className="checkbox" id="drama" name="drama" checked={genres.drama} onChange={(e) => handleCheck(e)} />
+          <label>Fantasy</label>
+          <input type="checkbox" className="checkbox" id="fantasy" name="fantasy" checked={genres.fantasy} onChange={(e) => handleCheck(e)} />
           <button type="submit">Save preferences</button>
         </form>
       </div>}
