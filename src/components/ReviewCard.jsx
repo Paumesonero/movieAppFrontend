72447187d331040740bcb19c582aa0ecedfb063e
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-19 00:57:47 +0200

    user details styles done

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import { NavLink} from 'react-router-dom';
-import { faHeartCrack } from '@fortawesome/free-solid-svg-icons';
 
 export default function ReviewCard(props) {
     const [errorMessage, setErrorMessage] = useState(undefined);
     const {review, onDelete, storedToken} = props;
     const [isLiked, setIsLiked] = useState(false);
     const [likeNumber, setLikeNumber] = useState(0)
+    
     useEffect(() => {
         const getIfIsLiked = async () => {
             try {
@@ -21,6 +21,7 @@ export default function ReviewCard(props) {
         }
         getIfIsLiked();
     },[isLiked,storedToken,review._id]);
+
     useEffect(() =>{
         const getNumber = async () =>{
             try {
@@ -34,7 +35,7 @@ export default function ReviewCard(props) {
     },[storedToken, review._id, likeNumber])
     const handleLike = async (reviewId) => {
         try {
-            await axios.post(`${process.env.REACT_APP_API_URL}/reviewLike/add/${reviewId}`, {}, { headers: { Authorization: `Bearer ${storedToken}` } });    
+            await axios.post(`${process.env.REACT_APP_API_URL}/reviewLike/${reviewId}/add`, {}, { headers: { Authorization: `Bearer ${storedToken}` } });    
             !isLiked && setLikeNumber(prev => prev +1)
             setIsLiked(prev => {return !prev})
         } catch (error) {
@@ -43,25 +44,29 @@ export default function ReviewCard(props) {
     };
     const handleRemoveLike = async (reviewId) => {
         try {
-            await axios.delete(`${process.env.REACT_APP_API_URL}/reviewLike/remove/${reviewId}`, { headers: { Authorization: `Bearer ${storedToken}` } });
+            await axios.delete(`${process.env.REACT_APP_API_URL}/reviewLike/${reviewId}/remove`, { headers: { Authorization: `Bearer ${storedToken}` } });
             isLiked && setLikeNumber(prev => prev -1)
             setIsLiked(prev => {return !prev})
         } catch (error) {
             setErrorMessage(error.response.data.error);
         }
     };
+
     return (
-        <div>
-            <div>
+        <div className='flex ml-3 mb-5'>
+            <div className='flex flex-col items-center'>
                 <NavLink to={`/movies/${review.movieId._id}`}><img src={review.movieId.translations[0].poster.og} alt="movie poster" className='w-12 min-w-[3rem] h-16 rounded-md' /></NavLink>
-                {!isLiked && <FontAwesomeIcon icon={faHeart} onClick={() => handleLike(review._id)}/>}
-                {isLiked && <FontAwesomeIcon icon={faHeartCrack} onClick={() => handleRemoveLike(review._id)}/>}
-                <p>{likeNumber}</p>
+                {!isLiked && <FontAwesomeIcon icon={faHeart} onClick={() => handleLike(review._id)} />}
+                {isLiked && <FontAwesomeIcon icon={faHeart} onClick={() => handleRemoveLike(review._id)} className='text-teal-500'/>}
+                {!isLiked && <p>{likeNumber}</p>}
+                {isLiked && <p className='text-teal-500'>{likeNumber}</p>}
+                
             </div>
-            <div>
+            <div className='ml-3'>
                 <p><strong>{review.titleReview}</strong></p>
                 <p>{review.review}</p>
-                <button onClick={() => onDelete(review._id, review.titleReview)}> Delete</button>
+                <button onClick={() => onDelete(review._id, review.titleReview)} className='relative mt-3 left-52 border-2 border-teal-500 border-opacity-50 px-3 py-px'> Delete</button>
+                <hr className='relative top-6 w-56 opacity-25'/>
             </div>
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         </div>
---
+++ src/views/user/UserOptions.jsx
@@ -0,0 +1,25 @@
+import React,{useContext} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../../context/AuthContext';
+import { NavLink} from 'react-router-dom';
+
+export default function UserOptions() {
+    const { isLoggedIn, logOutUser } = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
+  return (
+    <div className='h-screen'>
+        <div className='flex justify-end mr-4 text-3xl relative top-4'>
+         <NavLink to='/user'><FontAwesomeIcon icon={faXmark}/></NavLink>
+        </div>
+       
+        <div className='flex flex-col items-center justify-evenly h-3/4 '>
+            <NavLink to={`/user/preferences`} className='text-3xl border-b border-teal-600 px-2'>Preferences</NavLink>
+            <NavLink to="/edit-user" className='text-3xl border-b border-teal-600 px-2 '>Edit user</NavLink>
+            <NavLink to={'/my-reviews'} className='text-3xl border-b border-teal-600 px-2 '> All my reviews</NavLink>
+            {user && user.role === 'admin' && <NavLink to='/user-list' className='text-3xl border-b border-teal-600 px-2 '>See users</NavLink>} 
+            {isLoggedIn && <button onClick={() => logOutUser()} className='text-3xl border-b border-teal-600 px-2'>Log out</button>}
+        </div>
+    </div>
+  )
+}
