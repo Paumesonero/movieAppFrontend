6ed7a2d9ccd56166cd2a0ed357a7e3f9e54d8792
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-18 12:45:31 +0200

    user role fixed

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,6 +10,7 @@ export default function Home() {
   const [errorMessage, setErrorMessage] = useState(undefined);
   const [nextMovieId, setNextMovieId] = useState("");
   const {user} = useContext(AuthContext);
+
   useEffect(() => {
     const getNextMovie = async () => {
       try {
@@ -37,7 +38,7 @@ export default function Home() {
         </form>
         <NavLink to='/login'><img src="https://cdn-icons-png.flaticon.com/512/122/122662.png" alt="main-btn" /></NavLink>
       </div> }
-      {user.role === 'admin' && <NavLink to={`/movies/create`}>Create new Movie</NavLink>}
+      {user && user.role === 'admin' && <NavLink to={`/movies/create`}>Create new Movie</NavLink>}
     </div>
   )
 }
