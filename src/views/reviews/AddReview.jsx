87158fed170fad2d3df38c993ac59456ed21eb0a
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-21 19:25:55 +0200

    add reviews styles done

--- src/views/reviews/AddReview.jsx
+++ src/views/reviews/AddReview.jsx
@@ -47,18 +47,20 @@ export default function AddReview() {
     }
     return (
         <div className='h-screen mb-14'>
-            <h2>Add a review.</h2>
-            {movie && <img src={movie.translations[0].poster.og} alt='movie poster'></img>}
-            <form onSubmit={handleSubmit} className=''>
-                <div>
-                <label> Title</label>
-                <input type="text" name='titleReview' placeholder='Title' value={review.titleReview} onChange={handleChange} />
+            <div className='background-img polygon h-64 flex justify-center items-start'>
+                <h2 className='text-3xl mt-10'><strong>Add a <span className='text-[#65B3AD]'>review.</span> </strong> </h2>
+            </div>
+            {movie && <img src={movie.translations[0].poster.og} alt='movie poster' className='w-28 rounded-xl ml-5 relative bottom-32'></img>}
+            <form onSubmit={handleSubmit} className=' h-64 flex flex-col items-center gap-4 justify-center '>
+                <div className='flex flex-col w-3/4'>
+                    <label><strong className='text-xl'>Title</strong></label>
+                    <input type="text" name='titleReview' value={review.titleReview} onChange={handleChange} className='rounded h-11 w-full text-gray-200 bg-gray-700 text-lg px-3 focus:outline-teal-600' />
                 </div>
-                <div>
-                <label> Description</label>
-                <textarea name="review"  cols="30" rows="7" defaultValue={review.review} onChange={handleChange} />
+                <div className='flex flex-col w-3/4'>
+                    <label> <strong className='text-xl'>Description</strong></label>
+                    <textarea name="review"  cols="30" rows="7" defaultValue={review.review} onChange={handleChange} className='rounded text-gray-200 bg-gray-700 px-3 focus:outline-teal-600' />
                 </div>
-                <button type='submit'>Post</button>
+                <button type='submit' className='border-2 px-[7.5rem] py-3 mt-3 bg-[#65B3AD] border-[#65B3AD] rounded'> <strong className='text-lg text-gray-700'>Post</strong></button>
             </form>
         </div>
     );
